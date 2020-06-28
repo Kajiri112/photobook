d583d43d7ae097af8cad2bacce3e10cdc4a31ca2
Lisa Franzkowiak <kajiri@hotmail.de>
2020-06-28 10:26:00 +0200

    photopage & links

--- src/Pages/Content.js
+++ src/Pages/Content.js
@@ -2,31 +2,21 @@ import React from "react";
 import Main from "./Content/Main";
 import Photopage from "./Content/Photopage";
 import { Switch, Route } from "react-router-dom";
-import './Content.css'
+import "./Content.css";
 import About from "./Content/About";
 
 function Content() {
   return (
     <div className="content">
       <Switch>
-        <Route path="/about">
-          <About name="about" />
+        <Route exact path="/">
+          <Main />
         </Route>
-        <Route path="/users"></Route>
-        <Route path="/reisen">
-          <Photopage name="reisen" />
-        </Route>
-        <Route path="/figuren">
-          <Photopage name="figuren" />
-        </Route>
-        <Route path="/tiere">
-          <Photopage name="tiere" />
-        </Route>
-        <Route path="/diverses">
-          <Photopage name="diverses" />
+        <Route exact path="/about">
+          <About />
         </Route>
         <Route path="/">
-          <Main />
+          <Photopage />
         </Route>
       </Switch>
     </div>
--- src/Pages/Content/Photopage.js
+++ src/Pages/Content/Photopage.js
@@ -4,12 +4,14 @@ import "./Photopage.css";
 import Header from "../Header";
 import Lightbox from "react-image-lightbox";
 import "react-image-lightbox/style.css";
+import { useLocation } from "react-router-dom";
 
 function Photopage(props) {
-  
   const [images, setImages] = useState([]);
+  const [links, setLinks] = useState([]);
   const [photoIndex, setPhotoIndex] = useState(0);
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation().pathname;
 
   useEffect(() => {
     // Make a request for a user with a given ID
@@ -18,16 +20,34 @@ function Photopage(props) {
       .then(function (response) {
         // handle success
         let imagesReturn = [];
-        console.log(props.name);
-        console.log(response.data)
-
         for (let i = 0; i < response.data.length; i++) {
-          if(response.data[i].startsWith(props.name)) {
+          if (
+            response.data[i].startsWith(location) &&
+            response.data[i].lastIndexOf("/") == location.length
+          ) {
             let adress = "http://localhost:8081/" + response.data[i];
             imagesReturn.push(adress);
           }
         }
         setImages(imagesReturn);
+
+        // handle success
+        let linksReturn = [];
+
+        for (let i = 0; i < response.data.length; i++) {
+          if (response.data[i].startsWith(location)) {
+            let link = response.data[i];
+            link = link.substring(location.length + 1);
+            let slash = link.indexOf("/");
+            if (slash > -1) {
+              link = link.substring(0, slash);
+              if (!linksReturn.includes(link)) {
+                linksReturn.push(link);
+              }
+            }
+          }
+        }
+        setLinks(linksReturn);
       })
       .catch(function (error) {
         // handle error
@@ -37,21 +57,41 @@ function Photopage(props) {
 
   function getImageHTML() {
     let imagesReturn = [];
-  
+
     for (let i = 0; i < images.length; i++) {
-      let image = <img className="image" src={images[i]} key={i} onClick={() => {
-        setPhotoIndex(i);
-        setIsOpen(true);
-      }} />;
+      let image = (
+        <img
+          className="image"
+          src={images[i]}
+          key={i}
+          onClick={() => {
+            setPhotoIndex(i);
+            setIsOpen(true);
+          }}
+        />
+      );
       imagesReturn.push(image);
     }
     return imagesReturn;
   }
 
+  function getLinkHTML() {
+    let linksReturn = [];
+
+    for (let i = 0; i < links.length; i++) {
+      let link = <div className="linkColumn"><a href={location + "/" + links[i]}><img className="linkPanel" alt={links[i]} src={"http://localhost:8081/" + links[i] + ".jpg"}></img></a></div>;
+      linksReturn.push(link);
+    }
+    return linksReturn;
+  }
+
+  console.log(images);
+
   return (
-    <div className="photopage">
+    <div className="photoPage">
       <Header />
-      {getImageHTML()}
+      <div>{getImageHTML()}</div>
+      <div className="linkContainer">{getLinkHTML()}</div>
       {isOpen && (
         <Lightbox
           mainSrc={images[photoIndex]}
